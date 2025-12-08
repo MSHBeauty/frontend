4ea09015383ba95d209e8f9b6960383a98d3f21c
Daler <dsarifkulo021@gmail.com>
2025-12-09 03:28:31 +0500

    f

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,7 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		optimizeCss: false,
-		turbo: false,
-	},
+	output: "standalone",
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
